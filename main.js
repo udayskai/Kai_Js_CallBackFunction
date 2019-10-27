ed29864742cdce1725c385f61291abc923cd5c3b
uday singh <[udayskai@gmail.com]>
2019-10-27 13:47:11 +0530

    Need practice

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+function declarationFunctionName ( valueAsParameter, functionAsParameter){
+
+    functionAsParameter(valueAsParameter)
+};
+
+
+
+declarationFunctionName("uday singh as a vlaue parameter",function(parametername){
+    console.log(parametername);
+})
